796effaabf707ba7e697923944eabaeb5b409501
Khojiakbar <harry.gafurov@gmail.com>
2024-05-23 05:18:30 +0500

    commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Card from './components/Card'
+import Carousel from './components/Carousel'
+import Navbar from './components/Navbar'
+
+function App() {
+
+
+  return (
+    <div className='mx-auto max-w-[1110px]'>
+      <Navbar/>
+      <Card/>
+    </div>
+  )
+}
+
+export default App
